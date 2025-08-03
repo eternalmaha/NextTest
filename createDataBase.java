4b61318b1ba75b6ac54169fa96f156c657c68661
Maha <maha.ahmed4peace@gmail.com>
2025-08-02 20:57:42 -0400

    creating and connecting my SQLite database to Java

---
+++ createDataBase.java
@@ -0,0 +1,26 @@
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class createDataBase {
+
+    public static void main(String[] args) {
+        
+        String url = "jdbc:sqlite:studentSessions.db";
+
+        try (var conn = DriverManager.getConnection(url)){
+            
+            if(conn != null){
+                var meta = conn.getMetaData();
+                System.out.println("The driver name is " + meta.getDriverName());
+                System.out.println("A new database has been created"); 
+            }
+
+        }
+        catch(SQLException e){
+            System.err.println(e.getMessage());
+        }
+
+
+    }
+    
+}
