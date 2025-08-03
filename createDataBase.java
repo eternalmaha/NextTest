cf9d5c612816b8f65f600c0a95ba181e4a5c74ca
Maha <maha.ahmed4peace@gmail.com>
2025-08-03 16:58:29 -0400

    Creating student table in SQLite database

--- createDataBase.java
+++ createDataBase.java
@@ -7,18 +7,34 @@ public class createDataBase {
         
         String url = "jdbc:sqlite:studentSessions.db";
 
+        String studentTable = """
+            CREATE TABLE IF NOT EXISTS students (
+            id INTEGER PRIMARY KEY AUTOINCREMENT, 
+            studentName TEXT NOT NULL,
+            lastTestDate TEXT,
+            numberOfSessions INTEGER DEFAULT 0
+         );
+        """ ;
+
         try (var conn = DriverManager.getConnection(url)){
+
             
             if(conn != null){
                 var meta = conn.getMetaData();
                 System.out.println("The driver name is " + meta.getDriverName());
-                System.out.println("A new database has been created"); 
+                System.out.println("Database connection established"); 
             }
+
+            var stmt = conn.createStatement();
+            stmt.execute(studentTable); 
+            System.out.println("Table 'students' created or already exists.");
 
         }
         catch(SQLException e){
             System.err.println(e.getMessage());
         }
+
+        
 
 
     }
